86afb809a8580d7abbe6cf6ac2b6b295db7662ce
William McMeans <wrmcmeans@gmail.com>
2016-01-26 11:28:13 -0500

    Finish basic auth methods for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,21 @@ class User < ActiveRecord::Base
     end
   end
 
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
   private
     def ensure_session_token
       self.session_token ||= User.generate_session_token
