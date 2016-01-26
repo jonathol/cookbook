a04cc1c177fcd28bd8830ff89bfb649c4faceabe
William McMeans <wrmcmeans@gmail.com>
2016-01-26 12:28:10 -0500

    Finish most of user_form view partial styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      sign_in(@user)
+      log_in!(@user)
     else
       render :new
     end
