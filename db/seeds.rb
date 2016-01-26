83233213c7919bba33369a81b21e6fa903d0dc0d
William McMeans <wrmcmeans@gmail.com>
2016-01-26 13:52:46 -0500

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,75 @@ will = User.new(
   password: "starwars",
   name: "William McMeans"
 )
+
+maureen = User.new(
+  email: "maureen@gmail.com",
+  password: "starwars",
+  name: "Maureen Griswold",
+)
+
+david = User.new(
+  email: "davidjones@gmail.com",
+  password: "starwars",
+  name: "David Jones"
+)
+
+sophie = User.new(
+  email: "sophied@gmail.com",
+  password: "starwars",
+  name: "Sophie DiIorio"
+)
+
+user1 = User.new(
+  email: "adam33@gmail.com",
+  password: "movies"
+)
+
+user2 = User.new(
+  email: "kylew90@comcast.net",
+  password: "ilovefood",
+  name: "Kyle"
+)
+
+user3 = User.new(
+  email: "funnychef12@hotmail.com",
+  password: "cooking"
+)
+
+user4 = User.new(
+  email: "melissa.c.lee@gmail.com",
+  password: "muffins",
+  name: "Melissa"
+)
+
+user5 = User.new(
+  email: "tsmith2010@live.com",
+  password: "@@secret@@"
+)
+
+user6 = User.new(
+  email: "thehammer50@aol.com",
+  password: "aol?!?!",
+  name: "Matt"
+)
+
+user7 = User.new(
+  email: "annerobbins@gmail.com",
+  password: "fooood"
+)
+
+user8 = User.new(
+  email: "aec1977@gmail.com",
+  password: "p455w0rd"
+)
+
+user9 = User.new(
+  email: "giantsfan88@gmail.com",
+  password: "vanilla",
+  name: "Paul Ramirez"
+)
+
+user10 = User.new(
+  email: "lspeterson@gmail.com",
+  password: "cupcakes"
+)
