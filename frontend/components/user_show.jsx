99278d4b896aa8cea3a5c3646d9bf7a4b937ce9a
wmcmeans <wrmcmeans@gmail.com>
2016-01-29 18:34:11 -0500

    Add redirect to users recipe-box path on user show page is no child route given

--- frontend/components/user_show.jsx
+++ frontend/components/user_show.jsx
@@ -3,6 +3,13 @@ var React = require('react'),
     RecipeIndexItem = require('./recipe_index_item');
 
 module.exports = React.createClass({
+  componentDidMount: function () {
+    if (!this.props.children) {
+      var userPath = this.props.location.pathname;
+      this.props.history.pushState(null, userPath + "/recipe-box");
+    }
+  },
+
   render: function () {
     return (
       <main className="user-information">
