91c02c96595219e429d37de531b5690a9911eca7
wmcmeans <wrmcmeans@gmail.com>
2016-01-29 10:06:02 -0500

    Style RecipeDetail header, center basic recipe information

--- frontend/components/recipe_detail.jsx
+++ frontend/components/recipe_detail.jsx
@@ -56,42 +56,47 @@ var RecipeDetail = React.createClass({
     });
 
     return (
-      <article
+      <section
         className="recipe-detail">
-        <h2
-          className="recipe-detail-title">
-          {this.state.recipe.title}
-        </h2>
-        <h3
-          className="recipe-detail-author">
-          {this.state.recipe.author}
-        </h3>
-        <p
-          className="recipe-detail-more-info">
-          <span
-            className="detail-cook-time">
-            {this.state.recipe.cook_time}
-          </span>
-          <span
-            className="detail-servings">
-            {this.state.recipe.servings}
-          </span>
-        </p>
-        <div className="recipe-description">
-          <p
-            className="recipe-description-text">
-            {this.state.recipe.description}
-          </p>
-        </div>
-        <div className="recipe-prep-ingredients">
-          <ul className="recipe-ingredients">
-            {ingredients}
-          </ul>
-          <ul className="recipe-steps">
-            {steps}
-          </ul>
-        </div>
-      </article>
+        <article className="recipe-article">
+          <h2
+            className="recipe-detail-title">
+            {this.state.recipe.title}
+          </h2>
+          <div
+            className="basic-recipe-info">
+            <h3
+              className="recipe-detail-author">
+              {this.state.recipe.author}
+            </h3>
+            <p
+              className="recipe-detail-more-info">
+              <span
+                className="detail-cook-time">
+                {this.state.recipe.cook_time}
+              </span>
+              <span
+                className="detail-servings">
+                {this.state.recipe.servings}
+              </span>
+            </p>
+          </div>
+          <div className="recipe-description">
+            <p
+              className="recipe-description-text">
+              {this.state.recipe.description}
+            </p>
+          </div>
+          <div className="recipe-prep-ingredients">
+            <ul className="recipe-ingredients">
+              {ingredients}
+            </ul>
+            <ul className="recipe-steps">
+              {steps}
+            </ul>
+          </div>
+        </article>
+      </section>
     );
   }
 });
