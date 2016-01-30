381d2c973525be8e5c1376a3a10f65aeea4ee159
App Academy Student <appacademy@Apps-Mac-mini-40.local>
2016-01-30 14:52:36 -0500

    Add RecipePhoto seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -104,6 +104,13 @@ muffins = Recipe.create!(
   servings: "12 muffins"
 )
 
+RecipePhoto.create!(
+  recipe: muffins,
+  large_url: "https://c1.staticflickr.com/7/6083/6150882978_e21655a053_o.jpg",
+  thumb_url: "https://c1.staticflickr.com/7/6083/6150882978_85047c479e_n.jpg",
+  credit: "jeffreyw"
+)
+
 Ingredient.create!(
   name: "cups whole wheat flour",
   quantity: "2 1/2",
@@ -190,6 +197,13 @@ mayo = Recipe.create!(
   servings: "1 cup"
 )
 
+RecipePhoto.create!(
+  recipe: mayo,
+  large_url: "https://c1.staticflickr.com/5/4099/4795862818_ac2c47cde2_z.jpg",
+  thumb_url: "https://c1.staticflickr.com/5/4099/4795862818_ac2c47cde2_n.jpg",
+  credit: "jules:stonesoup"
+)
+
 Ingredient.create!(
 name: "teaspoons Dijon",
 quantity: "1 1/2",
@@ -237,6 +251,13 @@ hot_sauce = Recipe.create!(
   author_id: will.id,
   cook_time: "5 minutes",
   servings: "3 cups"
+)
+
+RecipePhoto.create!(
+  recipe: hot_sauce,
+  large_url: "https://c2.staticflickr.com/8/7035/6637805197_74bae4838e_o.jpg",
+  thumb_url: "https://c2.staticflickr.com/8/7035/6637805197_b50e7a6b36_n.jpg",
+  credit: "jeffreyw"
 )
 
 Ingredient.create!(
@@ -288,6 +309,14 @@ smoothie = Recipe.create!(
   servings: "3 cups"
 )
 
+RecipePhoto.create!(
+  recipe: smoothie,
+  large_url: "https://c1.staticflickr.com/9/8542/8699152877_1d524027c1_z.jpg",
+  thumb_url: "https://c1.staticflickr.com/9/8542/8699152877_1d524027c1.jpg",
+  credit: "q3studio"
+)
+
+
 Ingredient.create!(
 name: "cups frozen strawberries",
 quantity: "2",
@@ -312,11 +341,18 @@ PreparationStep.create!(
   recipe: smoothie
 )
 
-sesame_noodles = Recipe.create!(
-  title: "Sesame Noodles",
+udon = Recipe.create!(
+  title: "Udon Noodle Soup",
   author: maureen,
-  cook_time: "20 minutes",
+  cook_time: "45 minutes",
   servings: "4 servings"
+)
+
+RecipePhoto.create!(
+  recipe: udon,
+  large_url: "https://c1.staticflickr.com/3/2075/1777735659_6da1611f07.jpg",
+  thumb_url: "https://c1.staticflickr.com/3/2075/1777735659_6da1611f07_n.jpg",
+  credit: "Young Sok Yun"
 )
 
 hummus = Recipe.create!(
@@ -326,10 +362,24 @@ hummus = Recipe.create!(
   servings: "4 cups"
 )
 
+RecipePhoto.create!(
+  recipe: hummus,
+  large_url: "https://c2.staticflickr.com/6/5326/7149255929_dee97c4bb7.jpg",
+  thumb_url: "https://c2.staticflickr.com/6/5326/7149255929_dee97c4bb7_n.jpg",
+  credit: "Evgeni Zotov"
+)
+
 popcorn = Recipe.create!(
   title: "Caramel Popcorn",
   author: sophie,
   cook_time: "30 minutes",
+)
+
+RecipePhoto.create!(
+  recipe: popcorn,
+  large_url: "https://c2.staticflickr.com/2/1310/648222091_bdda57a0e9.jpg",
+  thumb_url: "https://c2.staticflickr.com/2/1310/648222091_bdda57a0e9.jpg",
+  credit: "ulterior epicure"
 )
 
 burrito = Recipe.create!(
@@ -338,16 +388,37 @@ burrito = Recipe.create!(
   cook_time: "45 minutes"
 )
 
-cauliflower_parm = Recipe.create!(
-  title: "Cauliflower Parmesan",
+RecipePhoto.create!(
+  recipe: burrito,
+  large_url: "https://c1.staticflickr.com/9/8263/8628568071_737137f1c3_h.jpg",
+  thumb_url: "https://c1.staticflickr.com/9/8263/8628568071_ecbf18a51d_n.jpg",
+  credit: "jeffreyw"
+)
+
+eggplant_parm = Recipe.create!(
+  title: "Eggplant Parmesan",
   author: maureen,
   cook_time: "1 1/2 hours"
 )
 
+RecipePhoto.create!(
+  recipe: eggplant_parm,
+  large_url: "https://c2.staticflickr.com/8/7203/6928717425_a6099fd4a4_z.jpg",
+  thumb_url: "https://c2.staticflickr.com/8/7203/6928717425_a6099fd4a4_n.jpg",
+  credit: "Jean-François Chénier"
+)
+
 marinara = Recipe.create!(
-  title: "San Marzano Marinara",
+  title: "Weeknight Marinara",
   author: will,
   cook_time: "30 minutes"
+)
+
+RecipePhoto.create!(
+  recipe: marinara,
+  large_url: "https://c2.staticflickr.com/4/3777/13257080663_93bd76ccc7.jpg",
+  thumb_url: "https://c2.staticflickr.com/4/3777/13257080663_93bd76ccc7_n.jpg",
+  credit: "jeffreyw"
 )
 
 chicken_curry = Recipe.create!(
@@ -356,10 +427,31 @@ chicken_curry = Recipe.create!(
   cook_time: "1 hour"
 )
 
+RecipePhoto.create!(
+  recipe: chicken_curry,
+  large_url: "https://c2.staticflickr.com/8/7150/6780438801_1c6b4ee069_z.jpg",
+  thumb_url: "https://c2.staticflickr.com/8/7150/6780438801_1c6b4ee069_n.jpg",
+  credit: "Nadir Hashmi"
+)
+
 pancakes = Recipe.create!(
-  title: "Pecan Pancakes",
+  title: "Blueberry Pancakes",
   author: will,
   cook_time: "25 minutes"
+)
+
+RecipePhoto.create!(
+  recipe: pancakes,
+  large_url: "https://c2.staticflickr.com/4/3148/2686805971_0d0a98fcbd_o.jpg",
+  thumb_url: "https://c2.staticflickr.com/4/3148/2686805971_50b03612c7_n.jpg",
+  credit: "Young Sok Yun"
+)
+
+RecipePhoto.create!(
+  recipe: pancakes,
+  large_url: "https://c2.staticflickr.com/4/3148/2686805971_0d0a98fcbd_o.jpg",
+  thumb_url: "https://c2.staticflickr.com/4/3148/2686805971_50b03612c7_n.jpg",
+  credit: "Young Sok Yun"
 )
 
 pecan_pie = Recipe.create!(
@@ -369,11 +461,25 @@ pecan_pie = Recipe.create!(
   servings: "6-8 slices"
 )
 
+RecipePhoto.create!(
+  recipe: pecan_pie,
+  large_url: "https://c2.staticflickr.com/6/5010/5286276345_dfe77d4d6e_o.jpg",
+  thumb_url: "https://c2.staticflickr.com/6/5010/5286276345_c7fb99390b_n.jpg",
+  credit: "Pearl Pirie"
+)
+
 margaritas = Recipe.create!(
   title: "Frozen Margaritas",
   author: will,
   cook_time: "2 hours",
   servings: "8 drinks"
+)
+
+RecipePhoto.create!(
+  recipe: margaritas,
+  large_url: "https://c2.staticflickr.com/4/3661/3514397858_a281859c21_z.jpg",
+  thumb_url: "https://c2.staticflickr.com/4/3661/3514397858_a281859c21.jpg",
+  credit: "Robert S. Donovan"
 )
 
 pad_kee_mao = Recipe.create!(
@@ -383,11 +489,25 @@ pad_kee_mao = Recipe.create!(
   servings: "4 servings"
 )
 
+RecipePhoto.create!(
+  recipe: pad_kee_mao,
+  large_url: "https://c2.staticflickr.com/4/3281/3005960647_58da483d4b.jpg",
+  thumb_url: "https://c2.staticflickr.com/4/3281/3005960647_58da483d4b.jpg",
+  credit: "Young Sok Yun"
+)
+
 pupusas = Recipe.create!(
   title: "Bean and Cheese Pupusas",
   author: sophie,
   cook_time: "1 hour",
   servings: "About 15 pupusas"
+)
+
+RecipePhoto.create!(
+  recipe: pupusas,
+  large_url: "https://c2.staticflickr.com/4/3516/3716608676_c56e84fea9_o.jpg",
+  thumb_url: "https://c2.staticflickr.com/4/3516/3716608676_1abb383428.jpg",
+  credit: "cherrypatter"
 )
 
 gratin = Recipe.create!(
@@ -397,11 +517,25 @@ gratin = Recipe.create!(
   servings: "6 servings"
 )
 
+RecipePhoto.create!(
+  recipe: gratin,
+  large_url: "https://c1.staticflickr.com/7/6027/5970146365_336c8e03fa_z.jpg",
+  thumb_url: "https://c1.staticflickr.com/7/6027/5970146365_336c8e03fa_n.jpg",
+  credit: "JaBB"
+)
+
 pulled_pork_sammy = Recipe.create!(
   title: "Pulled Pork Sandwich",
   author: david,
   cook_time: "7 hours",
   servings: "6 sandwiches"
+)
+
+RecipePhoto.create!(
+  recipe: pulled_pork_sammy,
+  large_url: "https://c2.staticflickr.com/6/5042/5252935828_7427ca3865_o.jpg",
+  thumb_url: "https://c2.staticflickr.com/6/5042/5252935828_43b489834c_n.jpg",
+  credit: "jeffreyw"
 )
 
 falafel = Recipe.create!(
@@ -411,11 +545,25 @@ falafel = Recipe.create!(
   servings: "6 sandwiches"
 )
 
+RecipePhoto.create!(
+  recipe: falafel,
+  large_url: "https://c1.staticflickr.com/9/8519/8561341670_ac836845df_z.jpg",
+  thumb_url: "https://c1.staticflickr.com/9/8519/8561341670_ac836845df_n.jpg",
+  credit: "SweetOnVeg"
+)
+
 pumpkin_soup = Recipe.create!(
   title: "Pumpkin Black Bean Soup",
   author: will,
   cook_time: "1 hour 15 minutes",
   servings: "5 bowls"
+)
+
+RecipePhoto.create!(
+  recipe: pumpkin_soup,
+  large_url: "https://c1.staticflickr.com/3/2807/10598921744_ab650a0047_h.jpg",
+  thumb_url: "https://c1.staticflickr.com/3/2807/10598921744_ded00597bb_n.jpg",
+  credit: "jeffreyw"
 )
 
 sundae = Recipe.create!(
@@ -425,11 +573,25 @@ sundae = Recipe.create!(
   servings: "1 sundae"
 )
 
+RecipePhoto.create!(
+  recipe: sundae,
+  large_url: "https://c2.staticflickr.com/8/7426/9567951921_cebd604e8e_z.jpg",
+  thumb_url: "https://c2.staticflickr.com/8/7426/9567951921_cebd604e8e_n.jpg",
+  credit: "Foodie Baker"
+)
+
 risotto = Recipe.create!(
   title: "Risotto",
   author: will,
   cook_time: "1 hour",
   servings: "4 servings"
+)
+
+RecipePhoto.create!(
+  recipe: risotto,
+  large_url: "https://c1.staticflickr.com/9/8733/16751919278_0a14d3a102_h.jpg",
+  thumb_url: "https://c1.staticflickr.com/9/8733/16751919278_4a0f84fd88_n.jpg",
+  credit: "Luca Nebuloni"
 )
 
 super_pig = Recipe.create!(
@@ -439,11 +601,25 @@ super_pig = Recipe.create!(
   servings: "5 servings"
 )
 
+RecipePhoto.create!(
+  recipe: super_pig,
+  large_url: "https://c1.staticflickr.com/1/118/277782049_30c7889b67_z.jpg",
+  thumb_url: "https://c1.staticflickr.com/1/118/277782049_30c7889b67_n.jpg",
+  credit: "Christopher Aloi"
+)
+
 pad_thai = Recipe.create!(
   title: "Pad Thai",
   author: maureen,
   cook_time: "45 minutes",
   servings: "4 servings"
+)
+
+RecipePhoto.create!(
+  recipe: pad_thai,
+  large_url: "https://c1.staticflickr.com/9/8460/7885126030_cea22d0b96_h.jpg",
+  thumb_url: "https://c1.staticflickr.com/9/8460/7885126030_9128750abc_n.jpg",
+  credit: "Jean-François Chénier"
 )
 
 pelmeni = Recipe.create!(
@@ -453,6 +629,13 @@ pelmeni = Recipe.create!(
   servings: "24 dumplings"
 )
 
+RecipePhoto.create!(
+  recipe: pelmeni,
+  large_url: "https://c2.staticflickr.com/4/3083/2685579511_59f8f326ca_z.jpg",
+  thumb_url: "https://c2.staticflickr.com/4/3083/2685579511_59f8f326ca.jpg",
+  credit: "ais3n"
+)
+
 baba_ganoush = Recipe.create!(
   title: "Baba Ganoush",
   author: sophie,
@@ -460,11 +643,26 @@ baba_ganoush = Recipe.create!(
   servings: "4 cups"
 )
 
-ramen = Recipe.create!(
+RecipePhoto.create!(
+  recipe: baba_ganoush,
+  large_url: "https://c1.staticflickr.com/5/4093/5602938902_32a326143d_o.jpg",
+  thumb_url: "https://c1.staticflickr.com/5/4093/5602938902_9cf6d404d3_n.jpg",
+  credit: "Stijn Nieuwendijk"
+)
+
+
+wonton = Recipe.create!(
   title: "Wonton Soup",
   author: david,
   cook_time: "1 hour 30 minutes",
   servings: "4 servings"
+)
+
+RecipePhoto.create!(
+  recipe: wonton,
+  large_url: "https://c2.staticflickr.com/2/1497/23656052023_bdc853d5ab_h.jpg",
+  thumb_url: "https://c2.staticflickr.com/2/1497/23656052023_daffd5e6b9_n.jpg",
+  credit: "Tim Sackton"
 )
 
 RecipeSave.destroy_all
