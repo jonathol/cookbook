c56c079dc3548922575ca5d3ee97717761090ef6
wmcmeans <wrmcmeans@gmail.com>
2016-02-01 18:38:45 -0500

    Begin writing notes controller

---
+++ app/controllers/api/notes_controller.rb
@@ -0,0 +1,23 @@
+class Api::NotesController < ApplicationController
+  def create
+    # note_params = {
+    #   recipe_id: params[:recipe_id],
+    #   user: current_user,
+    #   parent_note_id: params[:parent_note_id]
+    # }
+    # @recipe_save = RecipeSave.create!(recipe_save_params)
+    # render json: { @recipe_save.recipe_id => @recipe_save.id }
+  end
+
+  def index
+    if current_user
+      @notes = Recipe.find(params[:recipe_id])
+        .notes
+        .where("private = 0 OR user_id = (?)", current_user.id)
+    else
+      @notes = Recipe.find(params[:recipe_id])
+        .notes
+        .where("private = 0")
+    end
+  end
+end
