4455eb61b309734dc15a6e0027f00693b2584ff3
wmcmeans <wrmcmeans@gmail.com>
2016-02-02 20:16:33 -0500

    Notes render again

--- frontend/components/note_index_item.jsx
+++ frontend/components/note_index_item.jsx
@@ -27,25 +27,46 @@ var NoteIndexItem = React.createClass({
   },
 
   render: function () {
-    var childNotes;
-    if (this.props.note.child_notes.length > 0) {
+    var createNote = function (note) {
+      var isParent = !note.parent_id;
+      var noteClass = isParent ? "note" : "note child-note";
+      var replyButton;
+      if (isParent) {
+        replyButton = (
+          <li
+            onClick={this.toggleReply}>
+            <Icon name="reply" />
+            Reply
+          </li>
+        );
+      }
 
-    }
-
-    var parentNote = (
-
-    );
-
-    var replyButton;
-    if (!this.props.note.parent_id) {
-      replyButton = (
-        <li
-          onClick={this.toggleReply}>
-          <Icon name="reply" />
-          Reply
-        </li>
+      return (
+        <section className={noteClass}>
+          <div className="note-details group">
+            <div className="note-author-name">
+              {note.author.name}
+            </div>
+            <div className="note-time">
+              {note.time_ago}
+            </div>
+          </div>
+          <p className="note-body">
+            {note.body}
+          </p>
+          <ul
+            className="note-interaction-links group">
+            {replyButton}
+            <li>
+              <Icon name="thumbs-up" />
+              Helpful
+            </li>
+          </ul>
+        </section>
       );
-    }
+    };
+
+    var parentNote = createNote(this.props.note);
 
     var replyForm;
     if (this.state.formActive) {
@@ -57,31 +78,21 @@ var NoteIndexItem = React.createClass({
       );
     }
 
+    var childNotes;
+    if (this.props.note.child_notes.length > 0) {
+      this.props.note.child_notes.map(function (note, idx) {
+        var child_note = createNote(note);
+        return <li key={idx}>{child_note}</li>;
+      });
+    }
+
     return (
       <section className="note-index-item">
-        <section className="note">
-          <div className="note-details group">
-            <div className="note-author-name">
-              {this.props.note.author.name}
-            </div>
-            <div className="note-time">
-              {this.props.note.time_ago}
-            </div>
-          </div>
-          <p className="note-body">
-            {this.props.note.body}
-          </p>
-          <ul
-            className="note-interaction-links group">
-            {replyButton}
-            <li>
-              <Icon name="thumbs-up" />
-              Helpful
-            </li>
-          </ul>
-        </section>
+        {parentNote}
         {replyForm}
-        {childNotes}
+        <ul>
+          {childNotes}
+        </ul>
       </section>
     );
   }
