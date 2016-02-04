56f50414a4d289cfce7f1ed9af7b0ce798794383
wmcmeans <wrmcmeans@gmail.com>
2016-02-04 16:34:01 -0500

    Clean up FeaturedRecipe code

--- frontend/components/featured_recipe.jsx
+++ frontend/components/featured_recipe.jsx
@@ -3,23 +3,11 @@ var React = require('react'),
 
 var FeaturedRecipe = React.createClass({
   componentDidMount: function () {
-    var originalWindowHeight = $(window).height();
-    var originalWindowWidth = $(window).width();
     var handleWindowResize = function () {
       var imageHeight = $('.featured-recipe-photo').height();
       var windowHeight = $(window).height();
-      // var width = $('.featured-recipe-photo').data('original-width') || this.originalWidth;
-      // var height = $('.featured-recipe-photo').data('original-height') || this.originalHeight;
 
-
-
-      // var oldTop = $('.featured-recipe-photo').css('top') || 0;
-      var newHeight = $(window).height();
-      var newWidth = $(window).width();
-      // $('.featured-recipe-photo').css('top', (oldTop - (oldWidth - newWidth) + "px"));
       var recipePhotoAnchor = ((windowHeight * 0.85) - imageHeight);
-      var posOfImageBottom = recipePhotoAnchor + imageHeight;
-      // debugger
       if (recipePhotoAnchor >= 0) {
         $(".featured-recipe").css('min-height', imageHeight + "px");
         $('.featured-recipe-photo').css('top', "0px");
@@ -27,15 +15,7 @@ var FeaturedRecipe = React.createClass({
         $('.featured-recipe').css('min-height', windowHeight * 0.85 + "px");
         $('.featured-recipe-photo').css('top', recipePhotoAnchor + "px");
       }
-
-
-
     }.bind(this);
-
-    // $('.featured-recipe').css('min-height', originalWindowHeight * 0.85);
-    // $('.featured-recipe-photo').css('top', (originalWindowHeight * 0.85) - originalImageHeight);
-
-    // debugger
     $(window).resize(handleWindowResize);
 
     handleWindowResize();
